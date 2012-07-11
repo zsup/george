169cf13db8049ce34776efb43fac83d7fc63c565
Zach Supalla <zsupalla@gmail.com>
2012-07-11 12:52:35 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Hello, George.
+===
+
+This app is the Rails app for SWITCH, the most awesomest home automation system ever.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+George::Application.config.secret_token = 'cc93278f02b8cfdde8efda91b107e6bb046665cd18c31d529246871e39e8ec297481d7023d868e92799755e9065e24c274f59e4ef5318cc6992b347e2dd43be2'
